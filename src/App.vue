4a7b10ab048c79d49d3e0720c760ea3f97f566b8
WellDone88 <kenneth.moren@live.no>
2026-05-02 15:42:54 +0200

    first-push

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Clock from './components/Clock.vue'
+import Weather from './components/Weather.vue'
+import TodoList from './components/TodoList.vue'
+import Timer from './components/Timer.vue'
+import Calendar from './components/Calendar.vue'
+</script>
+
+<template>
+  <h1>Soon</h1>
+  <p>Working on the site....</p>
+</template>
+
+<style scoped></style>
